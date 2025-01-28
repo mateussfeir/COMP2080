d14001fbce709ffe6332a3f7713666a28913d9db
Mateus Sfeir <mateussfeir@hotmail.com>
2025-01-28 18:05:05 -0500

    fixed InsertionSort

--- InsertionSort.java
+++ InsertionSort.java
@@ -3,52 +3,52 @@ import java.util.Arrays;
 public class InsertionSort {
 
     // Method to perform Insertion Sort in ascending order
-    public static void insertionSortAsc(int[] arr, int numItems) {
-        for (int i = 1; i < numItems; i++) {
-            int key = arr[i];  // The element to be inserted
-            int j = i - 1;
+    public static void insertionSortAsc(int[] list, int numItems) {
+        for (int start = 1; start < numItems; start++) {
+            int temp = list[start];  // The element to be inserted
+            int prev = start - 1;
 
-            // Shift elements of arr[0..i-1] that are greater than key to one position ahead
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
+            // Shift elements of list[0..start-1] that are greater than temp to one position ahead
+            while (prev >= 0 && list[prev] > temp) {
+                list[prev + 1] = list[prev];
+                prev--;
             }
 
-            // Insert key at the correct position
-            arr[j + 1] = key;
+            // Insert temp at the correct position
+            list[prev + 1] = temp;
         }
     }
 
     // Method to perform Insertion Sort in descending order
-    public static void insertionSortDesc(int[] arr, int numItems) {
-        for (int i = 1; i < numItems; i++) {
-            int key = arr[i];  // The element to be inserted
-            int j = i - 1;
+    public static void insertionSortDsc(int[] list, int numItems) {
+        for (int start = 1; start < numItems; start++) {
+            int temp = list[start];  // The element to be inserted
+            int prev = start - 1;
 
-            // Shift elements of arr[0..i-1] that are smaller than key to one position ahead
-            while (j >= 0 && arr[j] < key) {
-                arr[j + 1] = arr[j];
-                j--;
+            // Shift elements of list[0..start-1] that are smaller than temp to one position ahead
+            while (prev >= 0 && list[prev] < temp) {
+                list[prev + 1] = list[prev];
+                prev--;
             }
 
-            // Insert key at the correct position
-            arr[j + 1] = key;
+            // Insert temp at the correct position
+            list[prev + 1] = temp;
         }
     }
 
     public static void main(String[] args) {
-        int[] array1 = {12, 11, 13, 5, 6};
-        int[] array2 = {12, 11, 13, 5, 6};  // Copy of the original array for descending order
-        int numItems = array1.length;  // Define numItems based on array size
+        int[] list1 = {12, 11, 13, 5, 6};
+        int[] list2 = {12, 11, 13, 5, 6};  // Copy of the original array for descending order
+        int numItems = list1.length;  // Define numItems based on array size
 
-        System.out.println("Original Array: " + Arrays.toString(array1));
+        System.out.println("Original Array: " + Arrays.toString(list1));
 
         // Sorting in ascending order
-        insertionSortAsc(array1, numItems);
-        System.out.println("Sorted Array (Ascending): " + Arrays.toString(array1));
+        insertionSortAsc(list1, numItems);
+        System.out.println("Sorted Array (Ascending): " + Arrays.toString(list1));
 
         // Sorting in descending order
-        insertionSortDesc(array2, numItems);
-        System.out.println("Sorted Array (Descending): " + Arrays.toString(array2));
+        insertionSortDsc(list2, numItems);
+        System.out.println("Sorted Array (Descending): " + Arrays.toString(list2));
     }
 }
